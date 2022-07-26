c74c9615082f54549cb94d4740f4be57df479552
Netchanok <50243@cru.ac.th>
2022-07-26 14:36:45 +0700

    Function

---
+++ function.py
@@ -0,0 +1,14 @@
+#สร้างฟังก์ชัน
+def hello():
+    print("Hello World")
+    print("Netchanok")
+#เรียนใช้งานฟังก์ชัน
+hello()
+#ส่งค่าผ่านparameter
+def sum(x,y):
+    x + y
+
+    return "hello"
+
+print(sum(5,8))
+
